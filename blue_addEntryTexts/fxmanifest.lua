1b206b8dbac3249e36dc01aeb751342d490d020a
BlueStrike3307 <blueknight3307@gmail.com>
2021-07-03 00:15:52 +0200

    Add files via upload

---
+++ blue_addEntryTexts/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Timo Heuer'
+description 'Just about 200 addEntryTexts for your FiveM Server!'
+version '1.0'
+
+client_scripts {
+	'client.lua'
+}
+
